60b00342ccfcdd85f344016b52596c3fcb2d68ab
jpakkal <moises.md1@gmail.com>
2019-04-13 14:56:23 -0500

    Modulación por JS

--- assets/js/modulo.js
+++ assets/js/modulo.js
@@ -1,4 +1,6 @@
 $(function(){
   $("#header").load("header.html");
   $("#footer").load("footer.html");
+  $("#contenedor-1").load("contenedor-1.html");
+  $("#contenedor-2").load("contenedor-2.html");
 });
