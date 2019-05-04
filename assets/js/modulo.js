f0f2984b0d29b2747cc7968025236ca5ef829484
jpakkal <moises.md1@gmail.com>
2019-05-04 12:08:48 -0500

    Modulo de contacto y font awesome

--- assets/js/modulo.js
+++ assets/js/modulo.js
@@ -4,4 +4,5 @@ $(function(){
   $("#contenedor-1").load("contenedor-1.html");
   $("#contenedor-2").load("contenedor-2.html");
   $("#contenedor-3").load("contenedor-3.html");
+  $("#contenedor-4").load("contenedor-4.html");
 });
